8b7de1f4d6d383b742d6e68da7d43cd74f2b1bf1
Franklin <franklin.bdantas@hotmail.com>
2025-10-17 09:27:21 -0300

    Adds GameDataContext.java
    Signed-off-by: Franklin <franklin.bdantas@hotmail.com>

---
+++ src/main/java/com/ddsaveeditor/game/data/GameDataContext.java
@@ -0,0 +1,50 @@
+package com.ddsaveeditor.game.data;
+
+import com.ddsaveeditor.data.DataContext;
+import com.ddsaveeditor.data.ReadonlyDataCollection;
+import com.ddsaveeditor.data.io.IDataReader;
+import com.ddsaveeditor.game.quirk.Quirk;
+
+/**
+ * A DataContext that holds Game Data
+ */
+public class GameDataContext extends DataContext
+{
+    private ReadonlyDataCollection<Quirk> quirks;
+    /**
+     * Gets all quirks from the game
+     * @return All quirks from {@link GameDataContext#quirks}
+     */
+    public ReadonlyDataCollection<Quirk> getQuirks(){
+        return this.quirks;
+    }
+
+    /**
+     * Creates a GameDataContext and loads its data
+     * @param reader the reader responsible for returning the data
+     */
+    public GameDataContext(IDataReader reader) {
+        super(reader);
+        this.load();
+    }
+
+    /** {@inheritDoc} **/
+    public void load() {
+        try{
+            this.quirks = new ReadonlyDataCollection<>(
+                    this.reader.readList(
+                            "quirk_library",
+                            Quirk.class
+                    )
+            );
+        } catch (Exception e) {
+            //TODO: throw custom exception
+            throw new RuntimeException(e);
+        }
+    }
+
+    /** {@inheritDoc} **/
+    public void commit() {
+
+    }
+}
